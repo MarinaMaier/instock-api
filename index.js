2019a09d14c40eeee26093de5f750dce0f4451ec
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-12 19:28:23 -0400

    add folder structure, the basic routes, install dependencies, create env sample

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5001;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+
+// Express middleware
+app.use(express.json());
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use((req, _res, next) => {
+    next();
+});
+
+const warehousesRoutes = require('./routes/warehouses-routes');
+const inventoryRoutes = require('./routes/inventory-routes');
+
+// Configuring warehouses endpoints
+app.use('/warehouses', warehousesRoutes);
+// Configuring inventory endpoints
+app.use('/inventory', inventoryRoutes);
+
+app.listen(PORT, function () {
+    console.log(`Server is now listening at ${PORT}`);
+});
