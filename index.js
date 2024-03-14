24e3323db8d47fc6223599d113b17c3f74872c0d
kittimaratana <kittima.ratana@gmail.com>
2024-03-14 12:46:17 -0400

    Create API to get list of all inventory items and initialize routes and controllers for inventories

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
 const warehousesRoutes = require('./routes/warehouses-routes');
+const inventoriesRoutes = require('./routes/inventories-routes');
+
 // Express middleware
 app.use(express.json());
 app.use(cors({ origin: CORS_ORIGIN }));
@@ -19,7 +21,7 @@ app.use((req, _res, next) => {
 // // Configuring warehouses endpoints
 app.use('/api/warehouses', warehousesRoutes);
 // // Configuring inventory endpoints
-// app.use('/inventory', inventoryRoutes);
+app.use('/api/inventories', inventoriesRoutes);
 
 app.get('/', (_req, res) => {
     res.send('Welcome to InStock API');
