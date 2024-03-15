6e40625e5c2daca9b12a8746564220845fbb769d
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-14 20:59:50 -0400

    add delete function to the Inventory Item

--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const inventoriesController = require('../controllers/inventories-controller');
 
 router.route("/").get(inventoriesController.index);
-router.route("/:id").get(inventoriesController.findOne);
+router.route("/:id").get(inventoriesController.findOne).delete(inventoriesController.remove);
 
 module.exports = router;
