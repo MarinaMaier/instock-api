fc51abba4b362a89ff01af967c482c7b18f34f7b
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-14 20:54:55 -0400

    add update function, validation for the inventory Id, missing properties, invalid warehouse, quantity, successful editing

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -48,8 +48,50 @@ const findOne = async (req, res) => {
     });
   }
 };
+// PUT/EDIT an Inventory Item
+const update = async (req, res) => {
+  const { warehouse_id, item_name, description, category, status, quantity } = req.body;
+  try {
+    // Checking if the inventory item with the specified ID exists
+    const existingInventory = await knex("inventories").where({ id: req.params.id }).first();
+    if (!existingInventory) {
+      return res.status(404).json({ message: "Inventory ID not found" });
+    }
+    // Validating the request body for missing properties
+    if (!warehouse_id || !item_name || !description || !category || !status || !quantity) {
+      return res.status(400).json({ message: "Missing properties in the request body" });
+    }
+    // Checking if the warehouse_id value exists in the warehouses table
+    const existingWarehouse = await knex("warehouses").where({ id: warehouse_id }).first();
+    if (!existingWarehouse) {
+      return res.status(400).json({ message: "Warehouse is not selected. Please select the warehouse from the dropdown list." });
+    }
+    // Validating the format of the quantity
+    if (isNaN(quantity)) {
+      return res.status(400).json({ message: "Quantity must be a number" });
+    }
+    // Updating the inventory item in the database
+    await knex("inventories").where({ id: req.params.id }).update({
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity
+    });
+    // Retrieving the updated inventory item from the database
+    const updatedInventory = await knex("inventories").where({ id: req.params.id }).first();
+    // Returning the updated inventory item in the response
+    res.status(200).json(updatedInventory);
+  } catch (error) {
+    // Handling errors
+    console.error("Error updating inventory:", error);
+    res.status(500).json({ message: "Unable to update inventory item" });
+  }
+};
 
 module.exports = {
   index,
   findOne,
+  update,
 };
--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const inventoriesController = require('../controllers/inventories-controller');
 
 router.route("/").get(inventoriesController.index);
-router.route("/:id").get(inventoriesController.findOne);
+router.route("/:id").get(inventoriesController.findOne).patch(inventoriesController.update);
 
 module.exports = router;
